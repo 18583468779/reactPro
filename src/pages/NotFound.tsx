b304aa443bffe7805e47506b7c2b83e012509bf2
xiewen <1123654054@qq.com>
2023-05-22 21:30:07 +0800

    404页面完成

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react";
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
 
-const NotFound: FC = () => {
-  return <p>NotFound</p>;
-};
+const NotFound: FC = () => (
+  <div style={{ height: "100%", marginTop: "100px" }}>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link to={"/"}>
+          <Button type="primary">Back Home</Button>
+        </Link>
+      }
+    />
+  </div>
+);
 
 export default NotFound;
