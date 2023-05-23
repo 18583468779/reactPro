5e5872334407d9e4d54ebef09c37bf63d559e8cb
xiewen <1123654054@qq.com>
2023-05-23 16:27:24 +0800

    登录注册页面样式完成

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,64 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import { Typography, Space, Form, Input, Button, Checkbox } from "antd";
+import { UserAddOutlined } from "@ant-design/icons";
+
+import { REGISTER_PATHNAME } from "../router";
+
+import styles from "./Login.module.scss";
+
+const { Title } = Typography;
+
+const USERNAME_KEY = "USERNAME";
+const PASSWORD_KEY = "PASSWORD";
 
 const Login: FC = () => {
-  return <p>Login</p>;
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 6, span: 16 }}
+          >
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
