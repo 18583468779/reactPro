32ff4fc648b79a50ed07f386959651607426300f
xiewen <1123654054@qq.com>
2023-05-27 15:42:43 +0800

    axios 拦截添加token

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ResType } from "../global";
 import { message } from "antd";
+import { getToken } from "./userToken";
 
 const instance = axios.create({
   baseURL: "/api",
@@ -9,6 +10,15 @@ const instance = axios.create({
   },
   timeout: 10 * 1000,
 });
+
+//拦截器，带上token
+instance.interceptors.request.use(
+  (config) => {
+    config.headers["Authorization"] = `Bearer ${getToken()}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 instance.interceptors.response.use((res) => {
   const { data, errno, msg } = (res.data || {}) as ResType;
